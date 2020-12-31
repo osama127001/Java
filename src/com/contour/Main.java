88dc9eb8cde53d38a42b5b685a595c8a3f80b643
Osama Khan <52834187+gitosamakhan@users.noreply.github.com>
2020-12-31 12:58:52 +0500

    adding arrays and multi-dimensional arrays

--- src/com/contour/Main.java
+++ src/com/contour/Main.java
@@ -1,5 +1,7 @@
 package com.contour;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,5 +12,14 @@ public class Main {
         String s2 = new String("osama");
         System.out.println(s2 == s1); // false, == checks memory address, reference
         System.out.println(s1.equals(s2)); // true, check values
+
+        // Arrays
+        int[] arr1 = new int[] {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr1));
+
+        // Multi-dimensional arrays
+        int [][] arr = new int [2][2];
+        arr[0][0] = 4;
+        System.out.println(Arrays.deepToString(arr));
     }
 }
