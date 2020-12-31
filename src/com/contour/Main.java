ca6bc1bf81ec8bd96b0a509afa975eb1833da4d5
Osama Khan <52834187+gitosamakhan@users.noreply.github.com>
2020-12-31 12:38:06 +0500

    adding == and .equals difference

--- src/com/contour/Main.java
+++ src/com/contour/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        // Difference between == and .equals()
+        String s1 = new String("osama");
+        String s2 = new String("osama");
+        System.out.println(s2 == s1); // false, == checks memory address, reference
+        System.out.println(s1.equals(s2)); // true, check values
     }
 }
