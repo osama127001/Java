e8a66628a975f62055f9dba420dec208e6080fdb
osama <o4osamakhan@gmail.com>
2021-01-03 19:30:01 +0500

    adding stack class and functions

---
+++ src/com/contour/Stack.java
@@ -0,0 +1,30 @@
+package com.contour;
+
+public class Stack extends DataStructure implements IDataStructure{
+
+    // insert at end in stack
+    public void insert() {
+
+    }
+
+    // returns the length of the stack
+    public int length() {
+        return super.length();
+    }
+
+    // deletes an element at the end in stack
+    public void delete() {
+
+    }
+
+    // traversing the stack
+    public void travers() {
+        super.traverse();
+    }
+
+    // searching the stack
+    public int search(int data) {
+        return super.search(data);
+    }
+
+}
